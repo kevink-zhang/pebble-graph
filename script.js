3f0942a9056afee10e1e97757c169c7c006552f8
Glitch (hello-webpage) <none>
2019-09-18 19:20:46 +0000

    💷⛈ Checkpoint ./index.html:991139/320 ./script.js:991139/2342:6827812/246 ./style.css:991139/110

--- script.js
+++ script.js
@@ -1,5 +1,38 @@
 /* If you're feeling fancy you can add interactivity 
     to your site with Javascript */
+const c = document.querySelector("#c");
+const ctx = c.getContext("2d");
 
-// prints "hi" in the browser's dev tools console
-console.log('hi');
+class Neuron {
+  constructor(a,b) {
+    this.x = a;
+    this.y = b;
+    this.s = s;
+    this.out = [];
+    this.in = [];
+  }
+  draw() {
+    ctx.fillRect(this.x, this.y,this.s,this.s);
+  }
+  update() {
+    this.out.forEach(x=>x.update(this.in[0]+this.in[1]));
+  }
+}
+
+let t = 0;
+let neurons = new Neuron(20,10);
+
+function draw() {
+  ctx.clearRect(0,0,c.width,c.height);
+  
+  
+  for(let n of neurons) {
+    n.draw();
+  }  
+  
+  t++
+  window.requestAnimationFrame(draw);
+}
+
+
+draw();
