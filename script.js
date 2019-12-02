305db2afaefc799902fd8ce0f3f077787a5e542f
Glitch (neuro-game) <none>
2019-12-02 05:34:08 +0000

    🌦💼 Checkpoint ./script.js:991139/2

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ const sig_speed = 0.03; //speed of signal
 const backdrop = "#000000";
 const neuron_color = "#ffffff";
 const neuro_ref = 0.03; // refractory period decay
-const decay = 0.03; //neuron value decay rate
+const decay = 0.06; //neuron value decay rate
 const neuro_max = 3;
 const neuro_init_color = 100;
 
