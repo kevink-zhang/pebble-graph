7f08390751276bd6e06d59438336c5b93afecaad
Glitch (neuro-game) <none>
2019-12-11 20:34:53 +0000

    🎱🎟 Checkpoint ./index.html:6827812/2824 ./script.js:6827812/22404

--- script.js
+++ script.js
@@ -2,25 +2,30 @@ const c = document.querySelector("#c");
 const ctx = c.getContext("2d");
 
 function deepClone(obj, hash = new WeakMap()) {
-    // Do not try to clone primitives or functions
-    if (Object(obj) !== obj || obj instanceof Function) return obj;
-    if (hash.has(obj)) return hash.get(obj); // Cyclic reference
-    try { // Try to run constructor (without arguments, as we don't know them)
-        var result = new obj.constructor();
-    } catch(e) { // Constructor failed, create object without running the constructor
-        result = Object.create(Object.getPrototypeOf(obj));
-    }
-    // Optional: support for some standard constructors (extend as desired)
-    if (obj instanceof Map)
-        Array.from(obj, ([key, val]) => result.set(deepClone(key, hash), 
-                                                   deepClone(val, hash)) );
-    else if (obj instanceof Set)
-        Array.from(obj, (key) => result.add(deepClone(key, hash)) );
-    // Register in hash    
-    hash.set(obj, result);
-    // Clone and assign enumerable own properties recursively
-    return Object.assign(result, ...Object.keys(obj).map (
-        key => ({ [key]: deepClone(obj[key], hash) }) ));
+  // Do not try to clone primitives or functions
+  if (Object(obj) !== obj || obj instanceof Function) return obj;
+  if (hash.has(obj)) return hash.get(obj); // Cyclic reference
+  try {
+    // Try to run constructor (without arguments, as we don't know them)
+    var result = new obj.constructor();
+  } catch (e) {
+    // Constructor failed, create object without running the constructor
+    result = Object.create(Object.getPrototypeOf(obj));
+  }
+  // Optional: support for some standard constructors (extend as desired)
+  if (obj instanceof Map)
+    Array.from(obj, ([key, val]) =>
+      result.set(deepClone(key, hash), deepClone(val, hash))
+    );
+  else if (obj instanceof Set)
+    Array.from(obj, key => result.add(deepClone(key, hash)));
+  // Register in hash
+  hash.set(obj, result);
+  // Clone and assign enumerable own properties recursively
+  return Object.assign(
+    result,
+    ...Object.keys(obj).map(key => ({ [key]: deepClone(obj[key], hash) }))
+  );
 }
 
 c.style.width = "500px";
@@ -53,56 +58,57 @@ class Node {
   drawNode() {
     ctx.strokeStyle = neutral_color;
     ctx.fillStyle = neutral_color;
-    if(this.sink){
+    if (this.sink) {
       ctx.fillStyle = back_color;
     }
-    if(this.v>=this.adj.length) ctx.strokeStyle = ctx.fillStyle = unstable_color;
-    
+    if (this.v >= this.adj.length)
+      ctx.strokeStyle = ctx.fillStyle = unstable_color;
+
     ctx.lineWidth = 2;
     ctx.beginPath();
-    ctx.arc(this.x,this.y,this.r,0,2*Math.PI);
+    ctx.arc(this.x, this.y, this.r, 0, 2 * Math.PI);
     ctx.stroke();
     ctx.closePath();
-    
-    if(this.sink) ctx.fillStyle = ctx.strokeStyle = back_color;
+
+    if (this.sink) ctx.fillStyle = ctx.strokeStyle = back_color;
     ctx.beginPath();
-    ctx.arc(this.x,this.y,this.r-4,0,2*Math.PI);
+    ctx.arc(this.x, this.y, this.r - 4, 0, 2 * Math.PI);
     ctx.fill();
     ctx.stroke();
     ctx.closePath();
-    
-    
+
     ctx.fillStyle = neutral_color;
-    if(this.v>=this.adj.length) ctx.fillStyle = unstable_color;
-    ctx.fillText(this.v,this.x+this.r+2,this.y-3);
+    if (this.v >= this.adj.length) ctx.fillStyle = unstable_color;
+    ctx.fillText(this.v, this.x + this.r + 2, this.y - 3);
     ctx.fillStyle = neutral_color;
-    ctx.fillText(this.adj.length,this.x+this.r+2,this.y+10);
-    ctx.fillText(this.tcount,this.x-this.r-7,this.y+10);
+    ctx.fillText(this.adj.length, this.x + this.r + 2, this.y + 10);
+    ctx.fillText(this.tcount, this.x - this.r - 7, this.y + 10);
   }
   drawEdge() {
-    for(let e of this.adj){
+    for (let e of this.adj) {
       ctx.strokeStyle = edge_color;
-      if(select!=null&&(select.id==this.id||select.id==e.id)) ctx.strokeStyle = select_color;
+      if (select != null && (select.id == this.id || select.id == e.id))
+        ctx.strokeStyle = select_color;
       ctx.lineWidth = edge_width;
       ctx.beginPath();
       ctx.moveTo(this.x, this.y);
-      ctx.lineTo(e.x ,e.y);
+      ctx.lineTo(e.x, e.y);
       ctx.stroke();
       ctx.closePath();
     }
   }
   addVal(v) {
-    this.v+=v;
+    this.v += v;
   }
-  addEdge(e){
+  addEdge(e) {
     this.adj.push(e);
   }
   update() {
-    if(this.sink) return false;
-    if(this.adj.length==0) return false;
-    
-    if(this.v>=this.adj.length){
-      this.v-=this.adj.length;
+    if (this.sink) return false;
+    if (this.adj.length == 0) return false;
+
+    if (this.v >= this.adj.length) {
+      this.v -= this.adj.length;
       this.tcount++;
       return true;
     }
@@ -111,31 +117,31 @@ class Node {
 }
 
 class Signal {
-  constructor(src,tar){
+  constructor(src, tar) {
     this.src = src;
     this.tar = tar;
-    this.pos = [src.x,src.y];
+    this.pos = [src.x, src.y];
     this.count = 25;
-    this.dx = (tar.x-src.x)/this.count;
-    this.dy = (tar.y-src.y)/this.count;
+    this.dx = (tar.x - src.x) / this.count;
+    this.dy = (tar.y - src.y) / this.count;
   }
   draw() {
     ctx.strokeStyle = unstable_color;
     //ctx.fillStyle = "black";
     ctx.lineWidth = 2;
     ctx.beginPath();
-    ctx.arc(this.pos[0],this.pos[1],2,0,2*Math.PI);
+    ctx.arc(this.pos[0], this.pos[1], 2, 0, 2 * Math.PI);
     ctx.fill();
     ctx.stroke();
     ctx.closePath();
   }
   update() {
-    this.pos[0]+=this.dx*sim_speed;
-    this.pos[1]+=this.dy*sim_speed;
-    this.count-=sim_speed;
-    if(this.count<=0){
+    this.pos[0] += this.dx * sim_speed;
+    this.pos[1] += this.dy * sim_speed;
+    this.count -= sim_speed;
+    if (this.count <= 0) {
       this.tar.addVal(1);
-      G.signals.splice(G.signals.indexOf(this),1);
+      G.signals.splice(G.signals.indexOf(this), 1);
       G.move = 50;
     }
   }
@@ -151,7 +157,7 @@ class Graph {
     this.unstable = false;
     this.finsim = false;
   }
-  reset(){
+  reset() {
     this.nodes = [];
     this.mem = new Map();
     this.signals = [];
@@ -163,29 +169,31 @@ class Graph {
     select = null;
   }
   draw() {
-    this.signals.forEach(x=>x.draw());
-    this.nodes.forEach(x=>x.drawEdge());
-    this.nodes.forEach(x=>x.drawNode());
-    if(this.src){ //checks if positive radius
+    this.signals.forEach(x => x.draw());
+    this.nodes.forEach(x => x.drawEdge());
+    this.nodes.forEach(x => x.drawNode());
+    if (this.src) {
+      //checks if positive radius
       ctx.strokeStyle = unstable_color;
       ctx.lineWidth = 1;
       ctx.beginPath();
-      ctx.arc(this.src.x,this.src.y,this.cnt/4,0,2*Math.PI);
+      ctx.arc(this.src.x, this.src.y, this.cnt / 4, 0, 2 * Math.PI);
       ctx.stroke();
       ctx.closePath();
     }
   }
-  addMemory(){
+  addMemory() {
     let mm = [];
-    this.nodes.forEach(x=>mm.push(x.v));
-    
+    this.nodes.forEach(x => mm.push(x.v));
+
     //console.log(mm);
-    if(this.mem[mm]) { //deja vu! i've been in this place before...
+    if (this.mem[mm]) {
+      //deja vu! i've been in this place before...
       this.finsim = true;
-      if(!this.unstable) scene = "add";
-      
-      for(let n of this.nodes){
-        if(n.v>=n.adj.length&&n.adj.length>0){
+      if (!this.unstable) scene = "add";
+
+      for (let n of this.nodes) {
+        if (n.v >= n.adj.length && n.adj.length > 0) {
           this.unstable = true;
           console.log("unstable");
           break;
@@ -195,34 +203,31 @@ class Graph {
     this.mem[mm] = true;
   }
   update() {
-    this.signals.forEach(x=>x.update());
-    
-    if(this.move<=0){
+    this.signals.forEach(x => x.update());
+
+    if (this.move <= 0) {
       this.src = null;
       this.cnt = 0;
-      
+
       this.addMemory();
-      if(scene=="add") return;
-      
-      for(let n of this.nodes){
-        if(n.update()){
+      if (scene == "add") return;
+
+      for (let n of this.nodes) {
+        if (n.update()) {
           this.move = "???";
           this.src = n;
           this.cnt = 40;
-          n.adj.forEach(x=>this.signals.push(new Signal(n,x)));
+          n.adj.forEach(x => this.signals.push(new Signal(n, x)));
           break;
         }
       }
-    }
-    else this.cnt+=sim_speed;
-    if(this.move>0) this.move-=sim_speed;
+    } else this.cnt += sim_speed;
+    if (this.move > 0) this.move -= sim_speed;
   }
-  addNode(x,y){
-    this.nodes.push(new Node(x,y));
+  addNode(x, y) {
+    this.nodes.push(new Node(x, y));
   }
-  setGraph(s){
-    
-  }
+  setGraph(s) {}
 }
 
 function dist(p1, p2) {
@@ -234,7 +239,6 @@ function fround(x, f) {
   return Math.floor(x * f) / f;
 }
 
-
 let t = 0; //time counter
 let st = 0; //simulation time
 let paused = false; //will not update graph
@@ -243,8 +247,7 @@ let select = null; //selected node
 let inSize = 0; //input graph generation size
 const slide_wid = 300;
 const slide_rad = 8;
-let slide_pos = c.width/2 - slide_wid/2;
-
+let slide_pos = c.width / 2 - slide_wid / 2;
 
 let G = new Graph();
 let H = [];
@@ -253,58 +256,64 @@ function draw() {
   //background
   ctx.fillStyle = back_color;
   ctx.fillRect(0, 0, c.width, c.height);
-  
+
   //update graph
-  if(scene=="play") {
+  if (scene == "play") {
     G.update();
   }
   //draw graph
   G.draw();
   //draw select animations
-  if(select!=null){
+  if (select != null) {
     ctx.strokeStyle = select_color;
     ctx.lineWidth = 1;
-    for(let i = 0; i < 3; i++){
+    for (let i = 0; i < 3; i++) {
       ctx.beginPath();
-      ctx.arc(select.x,select.y,select.r+4,i*2*Math.PI/3+t/10,i*2*Math.PI/3+2*Math.PI/3-0.65+t/10);
+      ctx.arc(
+        select.x,
+        select.y,
+        select.r + 4,
+        (i * 2 * Math.PI) / 3 + t / 10,
+        (i * 2 * Math.PI) / 3 + (2 * Math.PI) / 3 - 0.65 + t / 10
+      );
       ctx.stroke();
       ctx.closePath();
     }
   }
-  
+
   //slider
   ctx.fillStyle = "gray";
   ctx.beginPath();
-  ctx.moveTo(c.width/2-slide_wid/2,c.height-60);
-  ctx.lineTo(c.width/2-slide_wid/2,c.height-50);
-  ctx.lineTo(c.width/2+slide_wid/2,c.height-50);
-  ctx.lineTo(c.width/2+slide_wid/2,c.height-60);
+  ctx.moveTo(c.width / 2 - slide_wid / 2, c.height - 60);
+  ctx.lineTo(c.width / 2 - slide_wid / 2, c.height - 50);
+  ctx.lineTo(c.width / 2 + slide_wid / 2, c.height - 50);
+  ctx.lineTo(c.width / 2 + slide_wid / 2, c.height - 60);
   ctx.fill();
   ctx.closePath();
-  
+
   ctx.fillStyle = "white";
   ctx.beginPath();
-  ctx.arc(slide_pos,c.height-55,slide_rad,0,2*Math.PI);
+  ctx.arc(slide_pos, c.height - 55, slide_rad, 0, 2 * Math.PI);
   ctx.fill();
   ctx.closePath();
-  
+
   //information
   ctx.fillStyle = neutral_color;
-  ctx.fillText("mode: "+scene, 5, 10);
-  ctx.fillText("unstable: "+G.unstable, 5, 20);
-  ctx.fillText("time: "+t, 5, 30);
-  
+  ctx.fillText("mode: " + scene, 5, 10);
+  ctx.fillText("unstable: " + G.unstable, 5, 20);
+  ctx.fillText("time: " + t, 5, 30);
+
   t++;
   window.requestAnimationFrame(draw);
 }
 
-function presim(){
+function presim() {
   let tt = 0;
   scene = "play";
-  
+
   H = [deepClone(G)];
-  
-  while(!G.finsim){
+
+  while (!G.finsim) {
     G.update();
     H.push(deepClone(G));
     console.log(tt++);
@@ -320,7 +329,7 @@ let mPos = null;
 c.addEventListener("mousedown", e => {
   let x = e.clientX - c.getBoundingClientRect().left;
   let y = e.clientY - c.getBoundingClientRect().top;
-  if(dist([x,y],[slide_pos,c.height-55])<slide_rad) mPos = [x,y];
+  if (dist([x, y], [slide_pos, c.height - 55]) < slide_rad) mPos = [x, y];
 });
 
 let movedx = 0;
@@ -329,56 +338,54 @@ let movedy = 0;
 c.addEventListener("mousemove", e => {
   let x = e.clientX - c.getBoundingClientRect().left;
   let y = e.clientY - c.getBoundingClientRect().top;
-  if(mPos!=null){
-    mPos = [x,y];
+  if (mPos != null) {
+    mPos = [x, y];
   }
 });
 
-
 c.addEventListener("mouseup", e => {
   mPos = null;
-  
+
   let x = e.clientX - c.getBoundingClientRect().left;
   let y = e.clientY - c.getBoundingClientRect().top;
-  
-  let p = [x,y];
+
+  let p = [x, y];
   let newN = true;
-  for(let n of G.nodes){
-    let pp = [n.x,n.y];
-    if(dist(p,pp)<=n.r+1){
+  for (let n of G.nodes) {
+    let pp = [n.x, n.y];
+    if (dist(p, pp) <= n.r + 1) {
       newN = false;
-      if(keysdown[16]){
-        if(select!=null){
-          select.adj.splice(select.adj.indexOf(n),1);
-          n.adj.splice(n.adj.indexOf(select),1);
+      if (keysdown[16]) {
+        if (select != null) {
+          select.adj.splice(select.adj.indexOf(n), 1);
+          n.adj.splice(n.adj.indexOf(select), 1);
         }
-      }
-      else{
-        if(select==n) {
+      } else {
+        if (select == n) {
           select = null;
-        }
-        else if(select==null) select = n;
-        else{
+        } else if (select == null) select = n;
+        else {
           n.addEdge(select);
           select.addEdge(n);
           select = null;
         }
       }
     }
-    if(dist(p,pp)<=n.r*2) newN = false;
+    if (dist(p, pp) <= n.r * 2) newN = false;
   }
-  if(newN) {
-    G.addNode(x,y);
-    console.log("new node at: "+ x +" , "+ y);
+  if (newN) {
+    G.addNode(x, y);
+    console.log("new node at: " + x + " , " + y);
   }
-  
+
   //console.log(x + " " + y);
 });
 
 let keysdown = {};
 window.addEventListener("keydown", e => {
   const key = e.keyCode;
-  if(key == 16){ //shift key: yeeting edges
+  if (key == 16) {
+    //shift key: yeeting edges
     keysdown[16] = true;
   }
 });
@@ -386,53 +393,65 @@ window.addEventListener("keydown", e => {
 window.addEventListener("keyup", e => {
   const key = e.keyCode;
   console.log(key);
-  if(key==80){
+  if (key == 80) {
     presim();
     console.log("Finished");
   }
-  if(key==32){//space bar: toggles simulation
-    if(scene=="add") scene = "play";
-    else if(scene=="play") scene = "add";
+  if (key == 32) {
+    //space bar: toggles simulation
+    if (scene == "add") scene = "play";
+    else if (scene == "play") scene = "add";
   }
-  if(key== 16){//shift up: not yeeting edges
+  if (key == 16) {
+    //shift up: not yeeting edges
     keysdown[16] = false;
   }
-  if(key==82){ //r key: clear graph
+  if (key == 82) {
+    //r key: clear graph
     G.reset();
   }
-  if(select!=null && key==83){ //s key: toggles sink node
+  if (select != null && key == 83) {
+    //s key: toggles sink node
     select.sink = !select.sink;
     select = null;
   }
-  if(key==90 && select!=null){ //z key: deletes selected node
+  if (key == 90 && select != null) {
+    //z key: deletes selected node
     let ooo = select;
-    G.nodes.splice(G.nodes.indexOf(select),1);
-    for(let n of G.nodes){
-      for(let e of n.adj){
-        if(e.id==ooo.id){
-          n.adj.splice(n.adj.indexOf(e),1);
+    G.nodes.splice(G.nodes.indexOf(select), 1);
+    for (let n of G.nodes) {
+      for (let e of n.adj) {
+        if (e.id == ooo.id) {
+          n.adj.splice(n.adj.indexOf(e), 1);
         }
       }
     }
     select = null;
   }
-  if(key==69 && select!=null){ //e key: unselect node
+  if (key == 69 && select != null) {
+    //e key: unselect node
     select.addVal(1);
   }
-  
-  if(key==84){//t key: generate 
-    
+
+  if (key == 84) {
+    //t key: generate
   }
-  if(key==75){ //k key: generate complete
+  if (key == 75) {
+    //k key: generate complete
     G.reset();
-    let xx = c.width/2;
-    let yy = c.height/2;
-    let rr = c.height/2-50;
-    for(let i = 0; i < inSize; i++){
-      G.nodes.push(new Node(xx+Math.sin(2*Math.PI/inSize*i)*rr,yy+Math.cos(2*Math.PI/inSize*i)*rr));
+    let xx = c.width / 2;
+    let yy = c.height / 2;
+    let rr = c.height / 2 - 50;
+    for (let i = 0; i < inSize; i++) {
+      G.nodes.push(
+        new Node(
+          xx + Math.sin(((2 * Math.PI) / inSize) * i) * rr,
+          yy + Math.cos(((2 * Math.PI) / inSize) * i) * rr
+        )
+      );
     }
-    for(let i = 0; i < G.nodes.length-1; i++){
-      for(let j = i+1; j < G.nodes.length; j++){
+    for (let i = 0; i < G.nodes.length - 1; i++) {
+      for (let j = i + 1; j < G.nodes.length; j++) {
         G.nodes[i].adj.push(G.nodes[j]);
         G.nodes[j].adj.push(G.nodes[i]);
       }
@@ -446,8 +465,6 @@ const simspeed = document.getElementById("simspeed");
 inputsize.onchange = () => {
   inSize = inputsize.value;
 };
-simspeed.onchange = () =>{
-  sim_speed = simspeed.value/2;
-}
-
-
+simspeed.onchange = () => {
+  sim_speed = simspeed.value / 2;
+};
