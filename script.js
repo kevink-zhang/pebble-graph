7f7ea7ae82bf0b6d3ea12c301024d98c1f5f2935
Glitch (pebble-graph) <none>
2019-12-16 03:32:36 +0000

    🐣📮 Checkpoint ./script.js:6827812/119

--- script.js
+++ script.js
@@ -402,6 +402,8 @@ c.addEventListener("mousedown", e => {
   let x = e.clientX - c.getBoundingClientRect().left;
   let y = e.clientY - c.getBoundingClientRect().top;
   mPos = [x,y];
+  if(select!=null)
+    if(dist(mPos,[select.x,select.y])<select.r*2) mDrag = true;
 });
 
 let movedx = 0;
@@ -412,7 +414,7 @@ c.addEventListener("mousemove", e => {
   let y = e.clientY - c.getBoundingClientRect().top;
   
   if (mPos!=null) {
-    if(select!=null && dist([x,y])){ //dragging node
+    if(select!=null && mDrag){ //dragging node
       select.x = x-CAM.x;
       select.y = y-CAM.y;
     }
@@ -421,7 +423,7 @@ c.addEventListener("mousemove", e => {
       CAM.x+=x-mPos[0];
       CAM.y+=y-mPos[1];
     }
-    if(x!=mPos[0]||y!=mPos[1]) mDrag = true;
+    //if(x!=mPos[0]||y!=mPos[1]) mDrag = true;
     mPos = [x, y];
   }
 });
